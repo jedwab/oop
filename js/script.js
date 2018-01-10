f3caae06e16f38d30c00423be97ad7e26c51a5cf
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2018-01-10 16:40:29 +0100

    oop

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var iPhone6S = new Phone("Apple", 2250, "silver", 2015);
+var SamsungGalaxyS6 = new Phone("Samsung", 2789, "black", 2014);
+var OnePlusOne = new Phone("OnePlus", 1800, "white", 2018);
+
+function Phone(brand, price, color, productionYear) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+	this.productionYear = productionYear;
+}
+Phone.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " , the price is " + this.price + " and was made in year " + this.productionYear +".");
+}
+
+iPhone6S.printInfo ();
+SamsungGalaxyS6.printInfo ();
+OnePlusOne.printInfo ();

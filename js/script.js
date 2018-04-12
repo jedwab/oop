a8864f14043f6aa7ccd33fea8b501521c99b266f
Justyna Bobrowska <34577105+jedwab@users.noreply.github.com>
2018-04-12 13:46:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ function Button(text) {
 }
 Button.prototype = {
 	create: function() {
-var self = this;
+	var self = this;
 	this.$element = $('<button>');
 	this.$element.text(this.text);
 	this.$element.click(function() {
 		alert(self.text);
 		
 	});
-this.$element.appendTo($('body'));
+	this.$element.appendTo($('body'));
 	}
 }
 var btn1 = new Button('Hello!');
